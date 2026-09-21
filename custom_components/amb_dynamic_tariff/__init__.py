0ec9a858ecb09dd699912d7686d3b69e6a37e080
ianrossip <ianrossip@gmail.com>
2026-09-21 16:33:42 +0200

    Cancel tariff timers on unload

--- custom_components/amb_dynamic_tariff/__init__.py
+++ custom_components/amb_dynamic_tariff/__init__.py
@@ -22,7 +22,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
+    coordinator: AmbTariffCoordinator = hass.data[DOMAIN][entry.entry_id]
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
+        await coordinator.async_shutdown()
         hass.data[DOMAIN].pop(entry.entry_id, None)
     return unload_ok
